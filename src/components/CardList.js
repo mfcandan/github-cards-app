f623a0a8d5591eca6e00d6178139938b7de904cb
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-04-06 17:30:27 +0300

    finish app

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,7 +3,7 @@ import Card from './Card'
 export default function CardList(props) {
     return (
         <div>
-            {props.testData.map(profile => <Card {...profile}/>)}
+            {props.testData.map(profile => <Card key={profile.id} {...profile}/>)}
         </div>
     )
 }
