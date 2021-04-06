7166a98015a03d5437f7b6e2fd658565e3cee8ac
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-04-06 16:07:54 +0300

    initialize & setup card.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import React from 'react'
+import Card from './components/Card';
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+class App extends React.Component {
+  //constructer
+  //this
+
+  render(){
+    return( 
+      <>
+        <div className="header">{this.props.title}</div>
+        <Card />
+      </>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Card extends Component {
+    render() {
+        return (
+            <div className="github-profile">
+                <img src="https://placehold.it/75"/>
+                <div className="info">
+                    <div className="name">Name here..</div>
+                    <div className="company">Company here..</div>
+                </div>
+            </div>
+        )
+    }
+}
